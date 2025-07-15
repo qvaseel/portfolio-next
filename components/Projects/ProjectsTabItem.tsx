c70eeffca07a2ad7fd7ba470c141802870ae45bf
qvaseel <vasilina.makarkina@gmail.com>
2025-07-15 18:16:04 +0700

    Обновления: адаптивная верстка, правки формы

--- components/Projects/ProjectsTabItem.tsx
+++ components/Projects/ProjectsTabItem.tsx
@@ -10,7 +10,7 @@ interface IProjectsTabItem {
 const ProjectsTabItem = ({ title, imgSrc, link }: IProjectsTabItem) => (
     <div className={styles.projects_list_inner}>
         <a className={styles.projects_list_item_link} href={link} target='_blank'>
-            <Image className={styles.projects_list_item_img} src={imgSrc} width={544} height={422} alt={title} />
+            <Image className={styles.projects_list_item_img} src={imgSrc} alt={title} />
             <p className={styles.projects_list_item_title}>{title}</p>
         </a>
     </div>
